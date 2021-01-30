46eca4b8e9973fcd0d8f5b5882e56113278e6432
gianni <younggianniguy@gmail.com>
2021-01-30 12:57:34 -0600

    created functions for obtaining file paths and copying folders

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
+import sqlite3
+import os
+import shutil
+from pathlib import Path
+
+def obtainListOfPaths(path):
+    listOfFiles = list()
+    for (dirpath, dirname, filenames) in os.walk(path, topdown=True):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+    return listOfFiles
+
+
+
+#todo optimize this and make sure it checks if the directory needs to be created first
+def copyLibrary(source, destination):
+    print("copying from "+source+" to "+destination)
+    shutil.copytree(source, destination)
+    print("copying complete")
+
+
+
+tempPath = "/home/gianni/Documents/code/python/comicbooks"
+copyLibrary(tempPath, "/home/gianni/.comicOrchard/main")
 
